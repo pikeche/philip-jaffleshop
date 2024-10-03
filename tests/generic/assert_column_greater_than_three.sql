6dfbe3e6d229e250f82746fba1ec88f2406bb3d7
pikeche@phdata.io <pikeche@phdata.io>
2024-10-03 13:51:38 +0000

    more test updates

---
+++ tests/generic/assert_column_greater_than_three.sql
@@ -0,0 +1,10 @@
+{{ config(enabled = false) }}
+
+{% test assert_column_greater_than_three( model, column_name) %}
+
+select
+    {{ column_name }}
+from {{ model }}
+where {{ column_name }} <= 3
+
+{% endtest %}
