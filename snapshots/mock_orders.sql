44da1f5c12911361c4121344b3826e83de51bdd2
pikeche@phdata.io <pikeche@phdata.io>
2024-10-04 18:33:51 +0000

    snapshots

---
+++ snapshots/mock_orders.sql
@@ -0,0 +1,19 @@
+-- dbt 
+{% snapshot mock_orders %}
+
+{% set new_schema = target.schema + '_snapshot' %}
+
+{{
+    config(
+      target_database='sandbox',
+      target_schema=new_schema,
+      unique_key='order_id',
+
+      strategy='timestamp',
+      updated_at='updated_at'
+    )
+}}
+
+select * from {{ source('jaffle_shop','mock_orders') }}
+
+{% endsnapshot %}
